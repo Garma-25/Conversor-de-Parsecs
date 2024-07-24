7b70dbf6724f4e5a7656a696300be36e3515aeaa
João Gabriel Alves Cruz <110233476+Garma-25@users.noreply.github.com>
2024-07-24 03:08:21 -0300

    Add files via upload

---
+++ #conversor de parsecs para anos-luz.py
@@ -0,0 +1,9 @@
+#conversor de parsecs para anos-luz
+
+parsec_input = input('Número de parsecs: ')
+parsecs = int(parsec_input)
+anos_luz = 3.2616 * parsecs
+
+print(str(parsecs) + ' parsecs equivale a ' + str(anos_luz) + ' anos-luz. ')
+
+
